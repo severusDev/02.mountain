abd4333429ab637dd957c2a70217023fd5b8cbfc
severusDev <i.i.bystrov@yandex.ru>
2023-11-07 01:47:48 +0300

    Add scripts

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 
 let navLinks = document.querySelectorAll(".nav__link");
 let activeLink = document.querySelector(".nav__link--active");
+let eventItems = document.querySelectorAll(".event__item");
+
+
+// navigation
 
 navLinks.forEach(item => {
     item.addEventListener("click", (event) => {
@@ -17,5 +21,25 @@ function toggleLink(elem) {
 function removeLink() {
     navLinks.forEach(item => {
         item.classList.remove("nav__link--active")
+    });
+};
+
+
+// events
+
+eventItems.forEach(item => {
+    item.addEventListener("click", (event) => {
+        removeStyleEvent();
+        addStyleEvent(event.currentTarget);
+    });
+});
+
+function addStyleEvent(elem) {
+    elem.classList.add("event__item--active");
+};
+
+function removeStyleEvent() {
+    eventItems.forEach(item => {
+        item.classList.remove("event__item--active")
     });
 };
